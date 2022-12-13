b8ce304e4945a41c0cad1155d7d1e00f68e7460a
nurbek84 <nurbekkojjmberdiev84@gmail.com>
2022-12-13 18:04:02 +0400

    asdf

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
 
     }
 
+    public static void sortArray(int[] array) {
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+        }
+    }
 }
