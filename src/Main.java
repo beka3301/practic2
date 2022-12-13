acf9d5e78f3fbfb4b3868de0302d54ee22e1bbb3
beka3301 <bekturhanazarov98@gmail.com>
2022-12-13 20:00:20 +0600

    asd

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    }
+
+    public static void sumArray(int[] array) {
+        int sum = 0;
+        for (int a : array) {
+            sum += a;
+        }
+        System.out.println(sum);
+
     }
 }
